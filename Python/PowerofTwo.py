6152e4f11b8ef9fc1616d6eff1e7b301674ef49e
hotzan0301 <hotzan0301@gmail.com>
2022-11-19 23:02:52 +1000

    Done PowerofTwo.py

---
+++ Python/PowerofTwo.py
@@ -0,0 +1,10 @@
+def isPowerOfTwo(n: int) -> bool:
+    import math
+    if n < 0 or n == 0:
+        return False
+    if n == 1 or (math.log2(n) * 10 % 10) == 0.0:
+        return True
+    return False
+
+if __name__ == '__main__':
+    print(isPowerOfTwo(0))

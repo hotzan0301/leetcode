24816986aea91027e8c452e4fd50cf5621efdc0b
hotzan0301 <hotzan0301@gmail.com>
2022-11-21 21:17:09 +1000

    Update ReverseString.py

--- Python/ReverseString.py
+++ Python/ReverseString.py
@@ -1,14 +1,12 @@
 def reverseString(s):
     right = len(s) - 1
     left = 0
-    print(s)
     while (right > left):
         temp = s[right]
         s[right] = s[left]
         s[left] = temp
         right -= 1
         left += 1
-    print(s)
 
 if __name__ == "__main__":
     
